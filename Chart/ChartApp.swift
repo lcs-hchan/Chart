b1ed2eba9e865918908d402029c1797a8b448500
Hayden Chan <hchan@lcs.on.ca>
2024-11-04 08:44:00 -0500

    Initial Commit

---
+++ Chart/ChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartApp.swift
+//  Chart
+//
+//  Created by hayden on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct ChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartTests/ChartTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChartTests.swift
+//  ChartTests
+//
+//  Created by hayden on 2024-11-04.
+//
+
+import Testing
+@testable import Chart
+
+struct ChartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
